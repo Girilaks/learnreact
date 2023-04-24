faf6ded38ae7cbec6e040bd59072fa90034e5c13
Girilaks <vl.giri@yahoo.com>
2023-04-24 10:54:05 +0530

    first commit for react learning

---
+++ src/components/Classcomponent.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class Classcomponent extends Component {
+  render() {
+    return (
+      <div>This is my first class Component</div>
+    )
+  }
+}
+
+export default Classcomponent
+
